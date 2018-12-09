0f90052beaa60af72bf45f9fdb59b79395b1a9f0
Calvin Tantio <calvin.tantio@outlook.com>
2018-12-09 17:41:36 +0800

    Added functions for states

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+// Starting point (when the popup is loaded)
 window.addEventListener('load', function load(event) {
   const icons = document.querySelectorAll(".icon");
   for(var i = 0; i < icons.length; i++) {
@@ -6,29 +7,75 @@ window.addEventListener('load', function load(event) {
   }
 
   const applyButton = document.getElementById("applied");
-  applyButton.onclick = function() { toggleIconValue("applied") };
+  applyButton.onclick = toggleActivateButtonValue;
 });
+
+function toggleIconsClickable(clickable) {
+  const icons = document.querySelectorAll(".icon");
+  for(var i = 0; i < icons.length; i++) {
+    if(clickable) icons[i].classList.remove("unclickable");
+    else icons[i].classList.add("unclickable");
+  }
+}
+
+function toggleActivateButtonValue() {
+  const buttonId = "applied";
+
+  chrome.storage.sync.get(buttonId, function(result) {
+    var newButtonVal = !result[buttonId];
+
+    chrome.storage.sync.set({[buttonId]: newButtonVal}, function() {
+      if(newButtonVal) {
+        document.getElementById(buttonId).classList.add("active");
+      } else {
+        document.getElementById(buttonId).classList.remove("active");
+      }
+      toggleIconsClickable(!newButtonVal);
+    });
+
+  });
+}
+
+function toggleActivateButtonClickable(clickable) {
+  const buttonId = "applied";
+  if(clickable) {
+    document.getElementById(buttonId).classList.remove("unclickable");
+  } else {
+    document.getElementById(buttonId).classList.add("unclickable");
+  }
+}
+
+var activeIconsCount = 0;
 
 function toggleIconValue(iconId) {
   chrome.storage.sync.get(iconId, function(result) {
     var newIconVal = !result[iconId];
-    console.log(newIconVal);
+
     chrome.storage.sync.set({[iconId]: newIconVal}, function() {
       if(newIconVal) {
         document.getElementById(iconId).classList.add("active");
+        activeIconsCount++;
       } else {
         document.getElementById(iconId).classList.remove("active");
+        activeIconsCount--;
       }
+      toggleActivateButtonClickable(activeIconsCount > 0);
     });
+
   });
 }
 
 // Run once when popup first appears
 chrome.storage.sync.get(null, function(items) {
   const allKeys = Object.keys(items);
+
   for(var i = 0 ; i < allKeys.length; i++) {
     if(items[allKeys[i]]) {
       document.getElementById(allKeys[i]).classList.add("active");
+      if(allKeys[i] == "applied") toggleIconsClickable(false);
+      else activeIconsCount++;
     }
   }
+
+  toggleActivateButtonClickable(activeIconsCount > 0);
 });
