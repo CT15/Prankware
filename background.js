0437b13c2193512cb326c565f2ea0ed718ec18a1
Calvin Tantio <calvin.tantio@outlook.com>
2018-12-09 17:42:16 +0800

    Added blocking functionalities

--- background.js
+++ background.js
@@ -14,3 +14,48 @@ chrome.runtime.onInstalled.addListener(function() {
         console.log("Initialisation complete.")
     });
 });
+
+const iconUrlMap = {
+  facebook: "*://www.facebook.com/*",
+  spotify: "*://www.spotify.com/*",
+  youtube: "*://www.youtube.com/*",
+  instagram: "*://www.instagram.com/*",
+  github: "*://www.github.com/*",
+  reddit: "*://www.reddit.com/*",
+  twitter: "*://twitter.com/*",
+  twitch: "*://www.twitch.com/*",
+  pinterest: "*://www.pinterest.com/*"
+}
+
+function redirectRequest(details) {
+    const host = "https://github.com/CT15/Prankware";
+    return { redirectUrl: host };
+}
+
+var urls = [];
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for(key in changes) {
+    if(key == 'applied' && urls.length > 0) {
+      chrome.webRequest.onBeforeRequest.addListener(
+          redirectRequest,
+          { urls: urls,
+            types: [
+                    "main_frame",
+                    "sub_frame",
+                    "stylesheet",
+                    "script",
+                    "image",
+                    "object",
+                    "xmlhttprequest",
+                    "other"
+                  ]
+          },
+          ["blocking"]
+      );
+    } else if(key == 'applied' && urls.length <= 0) {
+      chrome.webRequest.onBeforeRequest.removeListener(redirectRequest);
+    } else {
+      urls.push(iconUrlMap[key]);
+    }
+  }
+});
